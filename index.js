b7af41ff65b0b311c9dac6d999779f9f0753b844
bechir mejri <badjo387@gmail.com>
2023-11-27 14:38:35 +0100

    anderung

---
+++ index.js
@@ -0,0 +1,8 @@
+import readlinesync from "readline-sync"
+const firstName ="bechir"
+console.log(firstName);
+const userName = readlinesync.question(`kannst du mir deine namen nennen`)
+console.log(`Hallo ${userName}, schon dich kennen zu lernennen`);
+const input = readlinesync.question(`wie alt bist du`)
+console.log(`Hallo ${userName}, bin 45`);
+
